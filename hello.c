c22e4f9753a5927b2f09c8e1400284ebb9e15390
j1yunn <yangjiyun1107@gmail.com>
2024-05-29 09:48:16 +0900

    Hello

--- hello.c
+++ hello.c
@@ -2,5 +2,12 @@
 
 int main() {
     printf("Hello, world!\n");
+
+int a = 100;
+
+if (a < 100) {
+    printf("하하하\n")
+}
+
     return 0;
 }
--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
 console.log("Hello, world!");
+
+let a = 100;
+
+if (a < 100) {
+    console.leg("하하하");
+}
