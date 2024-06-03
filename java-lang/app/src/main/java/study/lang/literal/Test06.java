49e6479e65ee76554decab0565e6920cbe45ab53
j1yunn <yangjiyun1107@gmail.com>
2024-06-03 15:52:46 +0900

    부동소수점 실습

---
+++ java-lang/app/src/main/java/study/lang/literal/Test06.java
@@ -0,0 +1,12 @@
+package study.lang.literal;
+
+// 실습
+// - 12.375f 부동 소수점을 컴파일 하라.
+// -
+
+public class Test06 {
+    public static void main(String[] args) {
+        float num = 12.375f;
+        System.out.println("부동 소수점 값: " + num);
+    }
+}
