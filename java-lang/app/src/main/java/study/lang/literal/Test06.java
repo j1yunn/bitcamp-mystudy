0e124bb4bc4c3af3df407290b5139c197be08d52
j1yunn <yangjiyun1107@gmail.com>
2024-06-04 11:42:14 +0900

    .

--- java-lang/app/src/main/java/study/lang/literal/Test06.java
+++ java-lang/app/src/main/java/study/lang/literal/Test06.java
@@ -1,12 +1,20 @@
 package study.lang.literal;
 
 // 실습
-// - 12.375f 부동 소수점을 컴파일 하라.
-// -
+// - 다음 파일을 컴파일하고 컴파일 된 클래스 파일을 16진수 코드로 읽어라.
+// - 문자, 정수, 부동소수점, 논리 값이 인코딩된 결과를 확인하라.
+//
 
 public class Test06 {
     public static void main(String[] args) {
-        float num = 12.375f;
-        System.out.println("부동 소수점 값: " + num);
+        // System.out.println("ABC\abc\n012\n가각간\n똘똠똥n"); // 41
+        System.out.println('Y'); // 41
+        System.out.println('간'); // 41
+        System.out.println('8'); // 41
+        System.out.println(1234); // 41
+        System.out.println(-1234); // 41
+        System.out.println(12.375f); // 41
+        System.out.println(true); // 41
+        System.out.println(false); // 41
     }
 }
