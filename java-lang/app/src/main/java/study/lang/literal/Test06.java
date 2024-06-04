d1b6f3409497e502eb9541bc76878bbc736d274a
j1yunn <yangjiyun1107@gmail.com>
2024-06-04 14:01:08 +0900

    이클립스

--- java-lang/app/src/main/java/study/lang/literal/Test06.java
+++ java-lang/app/src/main/java/study/lang/literal/Test06.java
@@ -7,14 +7,15 @@ package study.lang.literal;
 
 public class Test06 {
     public static void main(String[] args) {
-        // System.out.println("ABC\abc\n012\n가각간\n똘똠똥n"); // 41
-        System.out.println('Y'); // 41
-        System.out.println('간'); // 41
-        System.out.println('8'); // 41
-        System.out.println(1234); // 41
-        System.out.println(-1234); // 41
-        System.out.println(12.375f); // 41
-        System.out.println(true); // 41
-        System.out.println(false); // 41
+        // System.out.println("ABC\abc\n012\n가각간\n똘똠똥n"); // 41 42 43 0A 61 62 63 0A 39
+        // 31 32 0A EA B0 80
+        System.out.println('Y'); // 59
+        System.out.println('간'); // AC 04
+        System.out.println('8'); // 38
+        System.out.println(1234); // 2의 보수로 인코딩된 것을 확인한다. => 04 D2
+        System.out.println(-1234); // 2의 보수로 인코딩된 것을 확인한다. => FB 2E
+        System.out.println(12.375f); // 부동소수점이 메모리에 저장될 때 2진수로 바뀌는 것을 확인한다. => 41 46 00 00
+        System.out.println(true);
+        System.out.println(false);
     }
 }
