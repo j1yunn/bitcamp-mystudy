4caa51b2e88b1135af3389e6b637308e18053174
j1yunn <yangjiyun1107@gmail.com>
2024-06-05 12:40:37 +0900

    .

--- java-lang/app/src/main/java/Test6.java
+++ java-lang/app/src/main/java/Test6.java
@@ -1,17 +1,17 @@
 // 실습
 // - 애플리케이션 아규먼트를 다음과 같이 결과가 나오도록 모두 출력하라.
-//      $ java -classpath ... Test6 aaa bbb ccc ddd
-//      'aaa'
-//      'bbb'
-//      'ccc'
-//      'ddd'
+// $ java -classpath ... Test6 aaa bbb ccc ddd
+// 'aaa'
+// 'bbb'
+// 'ccc'
+// 'ddd'
 
 class Test6 {
-    public static void main(String[] args) {
-        for (String arg : args) {
-            System.out.println("'" + arg + "'");
-        }
+  public static void main(String[] args) {
+    for (String arg : args) {
+      System.out.println("'" + arg + "'");
     }
+  }
 }
 
 // class Test6 {
