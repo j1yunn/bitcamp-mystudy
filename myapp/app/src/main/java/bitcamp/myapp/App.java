f9bec015b370acc2c1e8940dbbcc5e536301e433
j1yunn <yangjiyun1107@gmail.com>
2024-06-10 16:44:47 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -14,53 +14,53 @@ public class App {
 
         String appTitle = "[팀 프로젝트 관리 시스템]";
         String line = "----------------------------------";
-        String menu1 = "1. 회원";
-        String menu2 = "2. 팀";
-        String menu3 = "3. 프로젝트";
-        String menu4 = "4. 게시판";
-        String menu5 = "5. 도움말";
-        String menu6 = "6. 종료";
+
+        String[] menus = new String[]{
+                "회원",
+                "팀",
+                "프로젝트",
+                "게시판",
+                "도움말",
+                "종료"};
 
         System.out.println(boldAnsi + line + resetAnsi);
         System.out.println(boldAnsi + appTitle + resetAnsi);
-        System.out.println(menu1);
-        System.out.println(menu2);
-        System.out.println(menu3);
-        System.out.println(menu4);
-        System.out.println(menu5);
-        System.out.println(boldAnsi + redAnsi + menu6 + resetAnsi);
+
+        for (int i = 0; i < menus.length; i++) {
+            if (menus[i] == "종료") {
+                System.out.printf("%s%d. %s%s\n", (boldAnsi + redAnsi), (i + 1), menus[i], resetAnsi);
+            } else {
+                System.out.printf("%d. %s\n", (i + 1), menus[i]);
+            }
+        }
+
+        System.out.println(boldAnsi + redAnsi + menus[5] + resetAnsi);
         System.out.println(boldAnsi + line + resetAnsi);
 
         int menuNo;
-        do {
+        while (true) {
             System.out.print("> ");
             menuNo = keyboardScanner.nextInt();
 
-            switch (menuNo) {
-                case 1:
-                    System.out.println("회원");
+            if (menuNo >= 1 && menuNo <= menus.length) {
+                if (menus[menuNo - 1] == "종료") {
                     break;
-                case 2:
-                    System.out.println("팀");
-                    break;
-                case 3:
-                    System.out.println("프로젝트");
-                    break;
-                case 4:
-                    System.out.println("게시판");
-                    break;
-                case 5:
-                    System.out.println("도움말");
-                    break;
-                case 6:
-                    System.out.println("종료합니다.");
-                    break;
-                default:
-                    System.out.println("메뉴 번호가 옳지 않습니다.");
-                    break;
-            }
-
-        } while (menuNo != 6);
+                }
+                System.out.println(menus[menuNo - 1]);
+        } else {
+            System.out.println("메뉴 번호가 옳지 않습니다.");
+        }
 
     }
-}
+
+
+        System.out.println("종료합니다.");
+
+        // 사용을 완료한 자원은 반환해야 다른 프로세스(프로그램)이 사용할 수 있다.
+        // 단, JVM을 종료하면 JVM이 사용한 모든 자원은 강제 회수된다.
+        // OS가 강제 회수한다.
+
+        keyboardScanner.close();
+        }
+    }
+
