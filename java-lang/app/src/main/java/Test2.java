7b90c1dd4ee97e3e6793d12e3aa09e3acc728671
j1yunn <yangjiyun1107@gmail.com>
2024-06-11 15:56:25 +0900

    .

--- java-lang/app/src/main/java/Test2.java
+++ java-lang/app/src/main/java/Test2.java
@@ -5,11 +5,13 @@
 // - 컴파일 오류가 발생한다.
 // - public 클래스가 있는 경우 소스 파일명은 해당 클래스 이름과 같아야 한다.
 
-public class X {
+class X {
 }
+
 
 class Y {
 }
+
 
 class Z {
 }
--- java-lang/app/src/main/java/study/lang/variable/Test.java
+++ java-lang/app/src/main/java/study/lang/variable/Test.java
@@ -1,4 +1,4 @@
-package study.lang;
+package study.lang.variable;
 
 public class Test {
     public static void main(String[] args) {
