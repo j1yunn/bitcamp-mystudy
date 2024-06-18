992174b359644d790a8dc59572f633c1cb10dd96
j1yunn <yangjiyun1107@gmail.com>
2024-06-18 17:51:30 +0900

    .

---
+++ java-lang/app/src/main/java/thisisjava/ch01/sec08/Hello.java
@@ -0,0 +1,7 @@
+package thisisjava.ch01.sec08;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello, Java");
+  }
+}
---
+++ java-lang/app/src/main/java/thisisjava/ch03/package-info.java
@@ -0,0 +1,1 @@
+package thisisjava.ch03;
