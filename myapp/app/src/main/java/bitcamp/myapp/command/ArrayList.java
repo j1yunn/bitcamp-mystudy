665bed14172229d17834e60a6e61dbf3a89081d1
j1yunn <yangjiyun1107@gmail.com>
2024-06-25 12:51:59 +0900

    배열 증가

--- myapp/app/src/main/java/bitcamp/myapp/command/ArrayList.java
+++ myapp/app/src/main/java/bitcamp/myapp/command/ArrayList.java
@@ -8,7 +8,23 @@ public class ArrayList {
   private int size = 0;
 
   public void add(Object obj) {
+    if (size == list.length) {
+      growUp();
+    }
     list[size++] = obj;
+  }
+  
+  private void growUp() {
+    int oldSize = list.length;
+    int newSize = oldSize + (oldSize >> 1); // 50% 증가
+
+    Object[] arr = new Object[newSize]; // 새 배열을 만든다.
+
+    for (int i = 0; i < list.length; i++) { //  기존 배열의 값을 복사해온다.
+      arr[i] = list[i];
+    }
+    list = arr; // 기존 배열의 주소를 버리고 새 배열의 주소를 담는다.
+
   }
 
   public Object remove(int index) {
