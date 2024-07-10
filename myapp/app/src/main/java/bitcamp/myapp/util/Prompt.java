d589864022f801e38ce5c37ec55445e6af452f5f
j1yunn <yangjiyun1107@gmail.com>
2024-07-10 16:21:30 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/util/Prompt.java
+++ myapp/app/src/main/java/bitcamp/myapp/util/Prompt.java
@@ -1,16 +1,19 @@
 package bitcamp.myapp.util;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Prompt {
 
   static Scanner keyboardScanner = new Scanner(System.in);
-  static Queue inputQueue = new Queue();
+  static Queue inputQueue = new LinkedList();
 
   public static String input(String format, Object... args) {
     String promptTitle = String.format(format + " ", args);
     System.out.print(promptTitle);
-    
+
     String input = keyboardScanner.nextLine();
     if (format.endsWith(">")) {
       inputQueue.offer(promptTitle + input); // 최근 명령어를 큐의 맨 뒤에 넣는다.
@@ -31,8 +34,9 @@ public class Prompt {
 
   public static void printHistory() {
     System.out.println("[명령 내역]----------------");
-    for (int i = 0; i < inputQueue.size(); i++) {
-      System.out.println(inputQueue.get(i));
+    Iterator iterator = inputQueue.iterator();
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
     }
     System.out.println("------------------------ 끝");
   }
