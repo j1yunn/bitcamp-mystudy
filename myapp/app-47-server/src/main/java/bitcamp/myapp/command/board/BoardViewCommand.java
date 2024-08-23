fd9001a5a5adc530a3b96e13409f8ae59421e3e3
j1yunn <yangjiyun1107@gmail.com>
2024-08-23 11:06:23 +0900

    .

--- myapp/app-47-server/src/main/java/bitcamp/myapp/command/board/BoardViewCommand.java
+++ myapp/app-47-server/src/main/java/bitcamp/myapp/command/board/BoardViewCommand.java
@@ -3,16 +3,15 @@ package bitcamp.myapp.command.board;
 import bitcamp.command.Command;
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
-import bitcamp.myapp.vo.User;
 import bitcamp.net.Prompt;
 import org.apache.ibatis.session.SqlSession;
 
-public class BoardUpdateCommand implements Command {
+public class BoardViewCommand implements Command {
 
   private BoardDao boardDao;
   private SqlSession sqlSession;
 
-  public BoardUpdateCommand(BoardDao boardDao, SqlSession sqlSession) {
+  public BoardViewCommand(BoardDao boardDao, SqlSession sqlSession) {
 
     this.boardDao = boardDao;
     this.sqlSession = sqlSession;
@@ -21,8 +20,6 @@ public class BoardUpdateCommand implements Command {
   @Override
   public void execute(String menuName, Prompt prompt) {
     try {
-      User loginUser = (User) prompt.getAttribute("loginUser");
-
       prompt.printf("[%s]\n", menuName);
       int boardNo = prompt.inputInt("게시글 번호?");
 
@@ -30,21 +27,21 @@ public class BoardUpdateCommand implements Command {
       if (board == null) {
         prompt.println("없는 게시글입니다.");
         return;
-      } else if (loginUser.getNo() > 10 && board.getWriter().getNo() != loginUser.getNo()) {
-        prompt.println("변경 권한이 없습니다.");
-        return;
       }
 
-      board.setTitle(prompt.input("제목(%s)?", board.getTitle()));
-      board.setContent(prompt.input("내용(%s)?", board.getContent()));
-
-      boardDao.update(board);
+      board.setViewCount(board.getViewCount() + 1);
+      boardDao.updateViewCount(board.getNo(), board.getViewCount());
       sqlSession.commit();
-      prompt.println("변경 했습니다.");
+
+      prompt.printf("제목: %s\n", board.getTitle());
+      prompt.printf("내용: %s\n", board.getContent());
+      prompt.printf("작성일: %1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS\n", board.getCreatedDate());
+      prompt.printf("조회수: %d\n", board.getViewCount());
+      prompt.printf("작성자: %s\n", board.getWriter().getName());
 
     } catch (Exception e) {
       sqlSession.rollback();
-      prompt.println("변경 중 오류 발생!");
+      prompt.println("조회 중 오류 발생!");
       e.printStackTrace();
     }
   }
